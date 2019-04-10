76672980dbcbb7a74ef1e2d499537554188cf7bf
leobello <leobello.wd@gmail.com>
2019-04-10 11:09:36 +0200

    latest

--- script.R
+++ script.R
@@ -3,3 +3,11 @@ VAPOR = read.csv("VAPOR.csv", header = TRUE)
 #RTUPB et VBBPS, methode similaire, peuvent être traité comme un seul ensemble de données
 RTUPB = read.csv("RTUPB.csv", header = TRUE)
 VBBPS = read.csv("VBBPS.csv", header = TRUE)
+
+labelVapor <- attributes(VAPOR)$row.names
+labelRTUPB <- attributes(RTUPB)$row.names
+labelVBBS <- attributes(VBBPS)$row.names
+
+boxplot(RTUPB)
+plot(RTUPB)
+plot(VAPOR)
