a67eb12409a0d4f3d18339833a9bf978c25ba0b7
leobello <leobello.wd@gmail.com>
2019-04-10 10:35:54 +0200

    début de projet

---
+++ script.R
@@ -0,0 +1,5 @@
+VAPOR = read.csv("VAPOR.csv", header = TRUE)
+
+#RTUPB et VBBPS, methode similaire, peuvent être traité comme un seul ensemble de données
+RTUPB = read.csv("RTUPB.csv", header = TRUE)
+VBBPS = read.csv("VBBPS.csv", header = TRUE)
