da3f784cc7658dc3934b1b50337b0ef3e717101a
leobello <leobello.wd@gmail.com>
2019-04-17 11:20:10 +0200

    adding plot

--- script.R
+++ script.R
@@ -10,9 +10,17 @@ preOpVap = VAPOR[,c(1:20)]
 
 preOpRtu = RTUPB[,c(1:20)]
 preOpVbb = VBBPS[,c(1:20)]
+
+#extraction des colonnes pour traitement post-opératoire
+#colonne 21 à 41
+postOpVap = VAPOR[,c(21:41)] 
+postOpRtu = RTUPB[,c(21:41)]
+postOpVbb = VBBPS[,c(21:41)]
+
+
 #concatenation car methode similaire (traitée comme un seul ensemble de données).
 preOp_RtETVb = rbind(preOpRtu, preOpVbb)
-
+postOp_RtETVb = rbind(postOpRtu, postOpVbb)
 
 ##recupère les labels des variables
 labelsPreOp = colnames(preOpVap)
@@ -41,15 +49,68 @@ labelsPreOpTraitee = c("Age",
                   "Reprise au bloc"
 )
 
+labelsPostOpTraitee = c("IPSS 1",
+                       "QoL 1",
+                       "Qmax 1 (ml/s)",
+                       "IPSS 2",
+                       "Qol 2",
+                       "Qmax 2 (ml/s)",
+                       "IPSS 3",
+                       "Qol 3",
+                       "Qmax 3 (ml/s)",
+                       "IPSS 4",
+                       "Qol 4",
+                       "Qmax 4 (ml/s)",
+                       "IPSS 5",
+                       "Qol 5",
+                       "Qmax 5 (ml/s)",
+                       "IPSS 6",
+                       "Qol 6",
+                       "Qmax 6 (ml/s)",
+                       "IPSS 7",
+                       "Qol 7",
+                       "Qmax 7 (ml/s)"
+)
+
 colnames(preOpRtu) <- labelsPreOpTraitee;
 colnames(preOpVap) <- labelsPreOpTraitee;
 colnames(preOpVbb) <- labelsPreOpTraitee;
+
+colnames(postOpRtu) <- labelsPostOpTraitee;
+colnames(postOpVap) <- labelsPostOpTraitee;
+colnames(postOpVbb) <- labelsPostOpTraitee;
 
 ##Analyse descriptive###
 
   ##Pré-op VAPOR 
     
 
+#Post-op VAPOR
 
+#évolution de l'IPSS
+boxplot(postOpVap$`IPSS 1`,
+        postOpVap$`IPSS 2`,
+        postOpVap$`IPSS 3`,
+        postOpVap$`IPSS 4`,
+        postOpVap$`IPSS 5`,
+        postOpVap$`IPSS 6`,
+        postOpVap$`IPSS 7`,
+        names=c(1,3,6,9,12,15,18),
+        main="Evolution de l'IPSS post-opération",
+        xlab="Mois de mesure de l'IPSS")
+
+#évolution de la moyenne de Qol
+
+#extraction de toutes les mesures IPSS
+ipssVap = postOpVap[,seq(1,21,3)];
+#moyennes des IPSS (deuxième argument 1 pour ligne 2 pour colonne)
+moyennes = apply(ipssVap,2,mean);
+
+
+# plot(postOpVap$`IPSS 1`,
+#      postOpVap$`IPSS 2`,
+#      xlab = "IPSS 1",
+#      ylab = "IPSS 2",
+#      type = "l")
 
 ## pour les dissimilarité, utilisé daisy (pour donnée hétérogène)
