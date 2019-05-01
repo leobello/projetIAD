a54e34d4c870b29e7c116a66a8f9f09262b600d2
tombarthelemy <tomir119@gmail.com>
2019-05-01 22:45:10 +0200

    bonne fete du travail..

--- analyse_technique_VAPOR.R
+++ analyse_technique_VAPOR.R
@@ -1,29 +1,15 @@
 VAPOR = read.csv("data/VAPOR.csv", header = TRUE)
 
-#RTUPB et VBBPS, methode similaire, peuvent être traité comme un seul ensemble de données
-VBBPS = read.csv("data/VBBPS.csv", header = TRUE)
-RTUPB = read.csv("data/RTUPB.csv", header = TRUE)
-
 ##extraction des colonnes pour traitement pré-operatoire 
 #(seulement colonne 1 à 20 : de "age" à "reprise au bloc"
 preOpVap = VAPOR[,c(1:20)]
-preOpRtu = RTUPB[,c(1:20)]
-preOpVbb = VBBPS[,c(1:20)]
 
 #extraction des colonnes pour traitement post-opératoire
 #colonne 21 à 41
 postOpVap = VAPOR[,c(21:41)] 
-postOpRtu = RTUPB[,c(21:41)]
-postOpVbb = VBBPS[,c(21:41)]
-
-#concatenation car methode similaire (traitée comme un seul ensemble de données).
-preOp_RtETVb = rbind(preOpRtu, preOpVbb)
-postOp_RtETVb = rbind(postOpRtu, postOpVbb)
 
 #on eleve la colonne 14 qui correspond à la technique utilisée 
 preOpVap = preOpVap[,-14]
-preOp_RtETVb = preOp_RtETVb[,-14]
-
 
 ##réecrire les labels
 labelsPreOpTraitee = c("Age","Comorbidité cardio Vx",
@@ -36,25 +22,9 @@ labelsPreOpTraitee = c("Age","Comorbidité cardio Vx",
                        "Caillotage","Reprise au bloc"
 )
 
-labelsPostOpTraitee = c("IPSS 1","QoL 1","Qmax 1 (ml/s)",
-                        "IPSS 2","Qol 2","Qmax 2 (ml/s)",
-                        "IPSS 3","Qol 3","Qmax 3 (ml/s)",
-                        "IPSS 4","Qol 4","Qmax 4 (ml/s)",
-                        "IPSS 5","Qol 5","Qmax 5 (ml/s)",
-                        "IPSS 6","Qol 6","Qmax 6 (ml/s)",
-                        "IPSS 7","Qol 7","Qmax 7 (ml/s)"
-)
-
-
 colnames(preOpVap) <- labelsPreOpTraitee;
-colnames(preOp_RtETVb) <- labelsPreOpTraitee
 
-
-colnames(postOpRtu) <- labelsPostOpTraitee;
-colnames(postOpVap) <- labelsPostOpTraitee;
-colnames(postOpVbb) <- labelsPostOpTraitee;
-
-
+### correspondance valeurs des variables indication,..
 indication = c("Retention Vésicale Algue","Échec du traitement médical",
                "Retention vesicale chronique","insuffisance rénale",
                "Lithiase vésicale","diverticule",
@@ -64,6 +34,8 @@ anesthesie = c("Anesthésie Loco-régionale",
 evenement_HD = c("non","hypotension","bradycardie","malaise vagual")
 non_oui = c("non","oui")
 
+
+####################### Remplace valeurs num par character #############
 for(i in seq(1,length(anesthesie)))
   preOpVap$Anestésie[preOpVap$Anestésie==i]=anesthesie[i] 
 
@@ -80,22 +52,23 @@ for(i in seq(0,1)){
   preOpVap$`Reprise au bloc`[preOpVap$`Reprise au bloc`==i]<- non_oui[i+1]
   preOpVap$`Porteur de sonde`[preOpVap$`Porteur de sonde`==i] <- non_oui[i+1]
 }
+##########################################################################
+
+resetDf <- function(){
+  VAPOR = read.csv("data/VAPOR.csv", header = TRUE)
+  preOpVap = VAPOR[,c(1:20)]
+  preOpVap = preOpVap[,-14]
+  
+  postOpVap = VAPOR[,c(21:41)] 
+  
+  colnames(preOpVap) <- labelsPreOpTraitee;
+}
 
 
 
+############################### Analyse descriptive ###############################
 
-##pré OP => état de santé des patients à l'arrivé (état + ou - en forme)
-## tous des cancers de la prostate avec degrée + ou - sevère
-## trouver des critères qui distinguent les groupes de patients
-
-##POST op => même combat
-
-##Analyse descriptive###
-
-  ##Pré-op VAPOR 
-#nature du jeu de donnée
 str(preOpVap)
-
 summary(preOpVap)
 
 median(preOpVap$Age)
@@ -111,15 +84,23 @@ qqnorm(preOpVap$Age)
 qqline(preOpVap$Age)
 #variable Age semble suivre une loi normal
 
-
 critere_oui_non_en_fonction_de_age = function(critere, nomCritere){
   bp = boxplot(preOpVap$Age ~ critere,
-               col = "purple", border = "black",
-               main = paste(nomCritere, "en fonction de l'âge"),
+               col = "khaki2", border = "black",
+               main = paste(nomCritere, ""),
                ylab = "âges[années]"
   )
   text(seq(ncol(bp$stats))+0.1, bp$stats[1, ] + 1, bp$n, col = "red") 
   tapply(preOpVap$Age, critere, mean)
+}
+critere_oui_non_en_fonction_duree = function(critere, nomCritere){
+  bp = boxplot(preOpVap$`Durée traitement médical (mois)` ~ critere,
+               col = "darkseagreen", border = "black",
+               main = paste(nomCritere, ""),
+               ylab = "durée[mois]"
+  )
+  text(seq(ncol(bp$stats))+0.1, bp$stats[1, ] + 1, bp$n, col = "red") 
+  tapply(preOpVap$`Durée traitement médical (mois)`, critere, mean)
 }
 
 par(mfrow=c(2,2))
@@ -128,91 +109,98 @@ critere_oui_non_en_fonction_de_age(preOpVap$Caillotage, "Présence de caillotage
 critere_oui_non_en_fonction_de_age(preOpVap$`Reprise au bloc`, "Reprise au bloc")
 critere_oui_non_en_fonction_de_age(preOpVap$`Comorbidité cardio Vx`, "Comorbidité")
 
+critere_oui_non_en_fonction_duree(preOpVap$`Porteur de sonde`, "Porteur de sonde")
+critere_oui_non_en_fonction_duree(preOpVap$Caillotage, "Présence de caillotage")
+critere_oui_non_en_fonction_duree(preOpVap$`Reprise au bloc`, "Reprise au bloc")
+critere_oui_non_en_fonction_duree(preOpVap$`Comorbidité cardio Vx`, "Comorbidité")
 
-
+par(mfrow=c(1,1))
 ##IPSS => plus c'est élevé, plus patient gené
-table(preOpVap$`IPSS P.O`)
-plot(jitter(preOpVap$Age), jitter(preOpVap$`IPSS P.O`))
+plot(jitter(preOpVap$Age), jitter(preOpVap$`IPSS P.O`), main="IPSS P.O", xlab="âge[années]", ylab="")
 #peut être lien ?
 
 ##QOL => plus c'est élevé, plus patient insatisfait
-table(preOpVap$`QoL P.O`)
-plot(jitter(preOpVap$Age), jitter(preOpVap$`QoL P.O`))
+plot(jitter(preOpVap$Age), jitter(preOpVap$`QoL P.O`), main="Qol P.O", xlab="âge[années]", ylab="")
 #peut être lien ?
 
 hist(preOpVap$`Durée traitement médical (mois)`,main="Distribution de la durée du traitement médical",
      ylab="Effectifs",xlab="nombre de mois", xaxt="n", ylim=c(0,15),col="darkred",  labels = TRUE)
 axis(1,seq(0,160,20))
 
-table(preOpVap$`Durée traitement médical (mois)`)
-plot(jitter(preOpVap$Age), jitter(preOpVap$`Durée traitement médical (mois)`))
+
+plot(jitter(preOpVap$Age), jitter(preOpVap$`Durée traitement médical (mois)`),main="Durée du traitement en fonction de l'âge", xlab="âge[années]", ylab="Durée traitement[mois]")
 #pas de lien visible
-
-
 
 
 ## pour les dissimilarité, utilisé daisy (pour donnée hétérogène)
 ## cmdscale
 library(cluster)
-par(mfrow=c(1,1))
 preOpVap = VAPOR[,c(1:20)]
 preOpVap = preOpVap[,-14]
 colnames(preOpVap) <- labelsPreOpTraitee;
 
-vap = preOpVap[preOpVap$`Porteur de sonde`==0,]
-#garder seulement les patients porteur de sonde
-
-par(mfrow=c(1,1))
 D=daisy(preOpVap, metric = "gower")
 
-#### CAH #####
-cah.ward <- hclust(D, method="ward.D2")
+par(mfrow=c(2,1))
+Cord2 = cmdscale(D)
+plot(Cord2, type="p")
+
+Cord3 = cmdscale(D, k=3)
+plot(Cord3[,1],Cord3[,3], type="p")
+
+## on affiche sur la premiere et troisieme dimensions pour avoir une autre vue. => permet d'avoir plus de recul sur certains
+## points semble parfois proche mais ne le sont pas tant que ça.
+
+############################### Clustering des données ###############################
+vap = preOpVap[preOpVap$`Porteur de sonde`==0,]
+#garder seulement les patients porteur de sonde
+vap <- lapply(vap, FUN = as.numeric)
+vap <- as.data.frame(vap)
+colnames(vap) <- labelsPreOpTraitee;
+vap$`Evenement H.D` = NULL
+vap$`Porteur de sonde` = NULL
+vap$`Transfusion PerO` = NULL
+
+vap.scale = scale(vap)
+
+A=daisy(vap.scale, metric = "euclidean")
+
+par(mfrow=c(2,1))
+Cord2 = cmdscale(A)
+plot(Cord2, type="p")
+
+Cord3 = cmdscale(A, k=3)
+plot(Cord3[,1],Cord3[,3], type="p")
+
+
+par(mfrow=c(1,1))
+cah.ward <- hclust(A, method="ward.D2")
 plot(cah.ward, cex = 0.7, hang = -1) # display dendrogram
-groupes.cah <- cutree(cah.ward, k=4) # cut tree into 4 clusters
+groupes.cah <- cutree(cah.ward, k=3) # cut tree into 3 clusters
 table(groupes.cah)
+# draw dendogram with red borders around the 3 clusters
+rect.hclust(cah.ward, k=3, border=2:5) 
+
+cah.average <- hclust(A, method="average")
+plot(cah.average, cex = 0.7, hang = -1) # display dendrogram
+groupes.average <- cutree(cah.average, k=4) # cut tree into 4 clusters
+table(groupes.average)
 # draw dendogram with red borders around the 4 clusters
-rect.hclust(cah.ward, k=4, border=2:5) 
+rect.hclust(cah.average, k=4, border=2:5) 
+
+## Partionning Around Medoids  PAM
+pam = pam(A, k=4)
+pam
+pam$silinfo
+plot(pam)
 
 
-#resAgnes <- agnes(D, method = "ward")
-#resAgnes$ac
-#pltree(resAgnes, cex = 0.6, hang = -1, main = "Dendrogram of agnes") 
-#grpA <- cutree(resHclus, k=4) # cut tree into 4 clusters
-#table(grpA)
-#rect.hclust(resAgnes, k=4, border=2:5) 
+pam3 = pam(A, k=3)
+pam3
+pam3$silinfo
+plot(pam3)
 
-#### K MEANS #####
-v = c(2:10)
-for(k in 2:10){
-  resu = kmeans(D, k)
-  a = resu$tot.withinss
-  b = resu$totss
-  v[k] = a/b
-}
-plot(v, type="l")
-
-groupes.kmeans = kmeans(D, 4, nstart = 5)
-groupes.kmeans
-clusplot(preOpVap, groupes.kmeans$cluster, main='2D representation of the Cluster solution',
-         color=TRUE, shade=TRUE,labels=2, lines=0)
-
-
-#### PAM #####
-groupes.pam = pam(preOpVap, 4)
-clusplot(pam(preOpVap, 4),  color=TRUE)
-
-table(groupes.cah, groupes.pam$clustering)
-
-
-
-
-## utilisé au moins de méthode pour faire découpage des classes
-## puis regardé si couple elem dans premier découpage se trouve dans une seul classe du deuxieme découpage ou pas
-## ex P1 : 4classes, P2: 3classes
-## confronter 2 partitions => rand(P1, P2)
-
-
-
+## on part sur 4 classes
 
 
 
